7e624b557121909e379a5c9501b7ddedac7d8172
Garimella Hari Pawan Kishore <garimellahari@gmail.com>
2023-01-23 15:51:13 +0530

    Added comments in download.py
    Added The comment of diffusers import and where to change the pipelines and HF model name

--- download.py
+++ download.py
@@ -4,7 +4,7 @@
 from diffusers import StableDiffusionPipeline, LMSDiscreteScheduler
 from diffusers import StableDiffusionInpaintPipeline
 from diffusers import DiffusionPipeline
-
+# from diffusers import [ADD YOUR PIPELINE HERE]
 import os
 
 
@@ -13,7 +13,7 @@ def download_model():
     #Set auth token which is required to download stable diffusion model weights
 #     HF_AUTH_TOKEN = os.getenv("HF_AUTH_TOKEN")
     HF_AUTH_TOKEN = "ADD YOUR AUTH TOKEN HERE"
-    
+#     CHANGE THE PIPELINE AND HF MODEL NAME BELOW.
     model = StableDiffusionInpaintPipeline.from_pretrained(
         "runwayml/stable-diffusion-inpainting",
         use_auth_token=HF_AUTH_TOKEN
